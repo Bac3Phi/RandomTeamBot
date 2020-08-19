0d6c88308b738f44706d235929de4ce412b2fb56
Hong Phuc <51514387+hongphuc5497@users.noreply.github.com>
2020-08-19 22:29:23 +0700

    WIP: Add randome_race (#3)
    * add random_race
    
    * Stopped tracking .env File
    
    * rm cache
    
    * Fix PR
    
    Co-authored-by: Bac3Phi <gkztest1@gmail.com>

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-require('dotenv').config()
-const commandRandom = require('./modules/random');
-const cat = require('./modules/cat');
-const prefix = '*';
+require("dotenv").config();
 
-client.on('ready', () => {
+const randomTeam = require("./modules/randomTeam");
+const randomRace = require("./modules/randomRace");
+const cat = require("./modules/cat");
+const prefix = "*";
+
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on("message", (msg) => {
   if (!msg.content.startsWith(prefix)) return;
 
-  const args = msg.content.slice(prefix.length).trim().split(' ');
+  const args = msg.content.slice(prefix.length).trim().split(" ");
   const command = args.shift().toLowerCase();
 
-  if (command === 'ping') {
-    msg.reply('xua con cat');
-  }else if (command === 'random') {
-    msg.channel.send(commandRandom(args));
-  }else if (command === 'cat') {
-    msg.reply( cat.command(args) );
-  }else if (command === 'help') {
-    msg.reply('hihi chưa implement help <3');
-  }else{
+  if (command === "ping") {
+    msg.reply("xua con cat");
+  } else if (command === "random_team") {
+    msg.channel.send(randomTeam(args));
+  } else if (command === "random_race") {
+    msg.channel.send(randomRace(args));
+  } else if (command === "cat") {
+    msg.reply(cat.command(args));
+  } else if (command === "help") {
+    msg.reply("hihi chưa implement help <3");
+  } else {
     msg.reply('xua con cat gì deo hiểu, "*help" đi bạn tôi ơi!');
   }
 });
-console.log("Token: ",process.env.token.substr(0,20)+"...");
+
 client.login(process.env.token);
--- modules/randomTeam.js
+++ modules/randomTeam.js
@@ -1,4 +1,4 @@
-module.exports = commandRandom = (args) => {
+module.exports = randomTeam = (args) => {
   let message = "";
 
   if (!args.length) {
