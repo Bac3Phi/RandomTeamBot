3c31cca1fae70d81510bf2e24e2e3483cd63cf93
Bac3Phi <gkztest1@gmail.com>
2020-08-19 15:30:42 +0700

    refactor random command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config()
+const commandRandom = require('./modules/random')
 const prefix = '*';
 
 client.on('ready', () => {
@@ -16,29 +17,7 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }else if (command === 'random') {
-    if (!args.length) {
-      return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
-    }
-    let team_size = args.shift()
-    let team_group = [];
-    let message = "";
-    
-    args.sort(function(){
-        return Math.round(Math.random()) - 0.5;
-    });
-
-    var totalTeams = Math.ceil(args.length/team_size);  
-    
-    var i,j,temparray,chunk = totalTeams;
-    for (i=0,j=args.length; i<j; i+=chunk) {
-        temparray = args.slice(i,i+chunk);
-        team_group.push(temparray);
-    }
-
-    team_group.map((team, index)=> {
-      message+=`Team ${index + 1}: ${team}\n`;
-    })
-    msg.channel.send(`Team group: ${team_size}\n${message}`);
+    msg.channel.send(commandRandom(args));
   }
 });
 client.login(process.env.token);
---
+++ modules/random.js
@@ -0,0 +1,28 @@
+module.exports = commandRandom = (args) => {
+  let message = "";
+
+  if (!args.length) {
+    return `You didn't provide any arguments`;
+  }
+
+  let team_size = args.shift();
+  let team_group = [];
+
+  args.sort(function () {
+    return Math.round(Math.random()) - 0.5;
+  });
+
+  var totalTeams = Math.ceil(args.length / team_size);
+
+  var i,j,temparray,chunk = totalTeams;
+  for (i = 0, j = args.length; i < j; i += chunk) {
+    temparray = args.slice(i, i + chunk);
+    team_group.push(temparray);
+  }
+
+  team_group.map((team, index) => {
+    message += `Team ${index + 1}: ${team}\n`;
+  });
+
+  return `Team group: ${team_size}\n${message}`;
+};
